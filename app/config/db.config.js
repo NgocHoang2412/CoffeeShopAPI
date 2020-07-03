c079e4a14b2eb85df494926c64dac75ad58b85a2
Vu Ngoc Hoang <ngochoang2412@gmail.com>
2020-07-03 17:19:47 +0700

    - add mongodb connect

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  url: "mongodb://localhost:27017/CoffeeShop",
+  //url: "mongodb://localhost:27017/CoffeeShop",
+  url: "mongodb+srv://vungochoang:123456%3Fq1@shophost-9hotc.mongodb.net/<dbname>?retryWrites=true&w=majority/CoffeeShop",
   jwt_secret: "myVerySecret",
   jwt_expiry_seconds: 36000,
 };
